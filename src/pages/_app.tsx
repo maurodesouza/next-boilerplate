6d4a2485d9aea7e248773b2c1f0fa79508f5e6c3
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-11-29 12:01:27 -0300

    📗  class 9 - add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../../styles/globals.css'
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import GlobalStyles from 'styles/global'
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/images/icon-512.png" />
+        <link rel="apple-touch-icon" href="/images/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
