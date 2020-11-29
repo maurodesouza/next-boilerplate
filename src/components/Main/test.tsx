aa5274ab535fdc69515aecdcd42dcbefb39ee5f0
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-11-29 10:52:47 -0300

    📕  class 7 - rtl config and first test

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,12 @@
+import { screen, render } from '@testing-library/react'
+import Main from '.'
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    render(<Main />)
+
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
+  })
+})
