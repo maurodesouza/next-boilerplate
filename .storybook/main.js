3a35bfad144387c547fe03610f8d52539a35e9af
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-05-03 18:50:34 -0300

    ⚡  chore: use absolute path in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,12 @@
 module.exports = {
-  "stories": [
-    "../src/components/**/stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../src/components/**/stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": ["@storybook/addon-essentials"]
+  addons: ['@storybook/addon-essentials'],
+
+  webpackFinal: config => {
+    config.resolve.modules.push(`${process.cwd()}/src`);
+
+    return config;
+  }
 }
