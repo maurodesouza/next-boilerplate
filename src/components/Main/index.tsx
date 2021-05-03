c9bef5dd1cba5bd28ce101f1d6fbe76a77409a24
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-05-03 18:53:47 -0300

    ⚡  chore: change wrapper name to container

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -4,7 +4,7 @@ const Main = ({
   title = 'React Avançado',
   description = 'TypeScript, ReactJS, NextJS e Styled Components'
 }) => (
-  <S.Wrapper>
+  <S.Container>
     <S.Logo
       src="/images/logo.svg"
       alt="Imagem de um átomo e React Avançado escrito ao lado."
@@ -15,7 +15,7 @@ const Main = ({
       src="/images/hero-illustration.svg"
       alt="Um desenvolvedor de frente para uma tela com código."
     />
-  </S.Wrapper>
-)
+  </S.Container>
+);
 
 export default Main
--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.main`
+export const Container = styled.main`
   background-color: #06092b;
   color: #fff;
   width: 100%;
